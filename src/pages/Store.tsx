ab68fc690b1387772504b241669ba3ecff765e1e
Dmitry <dmitry27zh@gmail.com>
2023-12-25 10:41:57 +0500

    Fill Store

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,5 +1,17 @@
+import { Col, Row } from 'react-bootstrap'
+import storeItems from '../data/items.json'
+
 const Store = () => {
-  return <div>Store</div>
+  return (
+    <>
+      <h1>Store</h1>
+      <Row className="g-3" md={2} xs={1} lg={3}>
+        {storeItems.map((item) => (
+          <Col>{JSON.stringify(item)}</Col>
+        ))}
+      </Row>
+    </>
+  )
 }
 
 export default Store
